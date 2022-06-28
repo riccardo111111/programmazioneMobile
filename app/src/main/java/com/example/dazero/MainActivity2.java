18dcff792a5df6ca7b0d8f822161bdd4bfbcf517
r.ruscittomarinel@studenti.unimol.it <p3p-C/?maxx/CgF>
2022-06-28 10:56:23 +0200

    da completare il salvataggio dei risultati

--- app/src/main/java/com/example/dazero/MainActivity2.java
+++ app/src/main/java/com/example/dazero/MainActivity2.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.ThumbnailUtils;
 import android.os.Build;
 import android.os.Bundle;
@@ -19,6 +20,7 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.dazero.ml.ModelTFLITE;
+import com.example.dazero.services.ResultService;
 
 import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
@@ -31,9 +33,24 @@ public class MainActivity2 extends AppCompatActivity {
 
     TextView result, confidence;
     ImageView imageView;
-    Button picture, home;
+    Button picture, home, save;
     int imageSize = 224;
-
+    Bitmap image;
+    int[] lista=new int[3];
+    String[] classes = {"Apple___Apple_scab", "Apple___Black_rot", "Apple___Cedar_apple_rust",
+            "Apple___healthy", "Blueberry___healthy", "Cherry_(including_sour)___Powdery_mildew",
+            "Cherry_(including_sour)___healthy", "Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot",
+            "Corn_(maize)___Common_rust_", "Corn_(maize)___Northern_Leaf_Blight", "Corn_(maize)___healthy",
+            "Grape___Black_rot", "Grape___Esca_(Black_Measles)", "Grape___Leaf_blight_(Isariopsis_Leaf_Spot)",
+            "Grape___healthy", "Orange___Haunglongbing_(Citrus_greening)", "Peach___Bacterial_spot",
+            "Peach___healthy", "Pepper,_bell___Bacterial_spot", "Pepper,_bell___healthy",
+            "Potato___Early_blight", "Potato___Late_blight", "Potato___healthy",
+            "Raspberry___healthy", "Soybean___healthy", "Squash___Powdery_mildew",
+            "Strawberry___Leaf_scorch", "Strawberry___healthy", "Tomato___Bacterial_spot",
+            "Tomato___Early_blight", "Tomato___Late_blight", "Tomato___Leaf_Mold",
+            "Tomato___Septoria_leaf_spot", "Tomato___Spider_mites Two-spotted_spider_mite",
+            "Tomato___Target_Spot", "Tomato___Tomato_Yellow_Leaf_Curl_Virus",
+            "Tomato___Tomato_mosaic_virus", "Tomato___healthy"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +62,7 @@ public class MainActivity2 extends AppCompatActivity {
         imageView = findViewById(R.id.imageView);
         picture = findViewById(R.id.button);
         home = findViewById(R.id.button4);
+        save = findViewById(R.id.button5);
 
 
         if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
@@ -82,6 +100,16 @@ public class MainActivity2 extends AppCompatActivity {
                 finish();
             }
         });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ResultService resultService= new ResultService(getApplicationContext());
+                int id = Integer.parseInt(getIntent().getStringExtra("id"));
+                resultService.createResult(0, id,  "2022-06-04", image.toString(),
+                        this.lista[0],);
+            }
+        });
     }
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -92,6 +120,7 @@ public class MainActivity2 extends AppCompatActivity {
             int dimension = Math.min(bitmap.getWidth(), bitmap.getHeight());
 
             bitmap = ThumbnailUtils.extractThumbnail(bitmap, dimension, dimension);
+            this.image=bitmap;
 
             imageView.setImageBitmap(bitmap);
 
@@ -147,24 +176,13 @@ public class MainActivity2 extends AppCompatActivity {
                     maxPos = i;
                 }
             }
-            String[] classes = {"Apple___Apple_scab", "Apple___Black_rot", "Apple___Cedar_apple_rust",
-                    "Apple___healthy", "Blueberry___healthy", "Cherry_(including_sour)___Powdery_mildew",
-                    "Cherry_(including_sour)___healthy", "Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot",
-                    "Corn_(maize)___Common_rust_", "Corn_(maize)___Northern_Leaf_Blight", "Corn_(maize)___healthy",
-                    "Grape___Black_rot", "Grape___Esca_(Black_Measles)", "Grape___Leaf_blight_(Isariopsis_Leaf_Spot)",
-                    "Grape___healthy", "Orange___Haunglongbing_(Citrus_greening)", "Peach___Bacterial_spot",
-                    "Peach___healthy", "Pepper,_bell___Bacterial_spot", "Pepper,_bell___healthy",
-                    "Potato___Early_blight", "Potato___Late_blight", "Potato___healthy",
-                    "Raspberry___healthy", "Soybean___healthy", "Squash___Powdery_mildew",
-                    "Strawberry___Leaf_scorch", "Strawberry___healthy", "Tomato___Bacterial_spot",
-                    "Tomato___Early_blight", "Tomato___Late_blight", "Tomato___Leaf_Mold",
-                    "Tomato___Septoria_leaf_spot", "Tomato___Spider_mites Two-spotted_spider_mite",
-                    "Tomato___Target_Spot", "Tomato___Tomato_Yellow_Leaf_Curl_Virus",
-                    "Tomato___Tomato_mosaic_virus", "Tomato___healthy"};
+
 
             result.setText(classes[maxPos]);
             String s = "";
-            int[] lista = {maxPos, second, third};
+            this.lista[0] =maxPos;
+            this.lista[1] =second;
+            this.lista[2] =third;
 
             for (int i = 0; i < 3; i++) {
                 s += String.format("%s: %.1f%%\n", classes[lista[i]], confidences[lista[i]] * 100);
