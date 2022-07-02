bbd20f8c844df73e36ecdd8f7c7d4c69abdb50cc
Er4ikk <g.zemignani@studenti.unimol.it>
2022-07-02 13:57:57 +0200

    added hash function

--- app/src/main/java/com/example/dazero/SingIn/SingInActivity.java
+++ app/src/main/java/com/example/dazero/SingIn/SingInActivity.java
@@ -16,6 +16,7 @@ import com.example.dazero.db.AppDatabase;
 import com.example.dazero.db.User;
 import com.example.dazero.services.ServiceManagerSingleton;
 import com.example.dazero.services.UserServices;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.commons.codec.digest.DigestUtils;
 
 
 public class SingInActivity extends AppCompatActivity {
@@ -48,7 +49,8 @@ public class SingInActivity extends AppCompatActivity {
                 dialog.show();
                 String email = binding.editTextEmail.getText().toString();
                 String password = binding.editTextPassword.getText().toString();
-
+                password = DigestUtils.sha384Hex(password);
+                Log.d("password",password);
                 if (email.isEmpty() || password.isEmpty()) {
                     dialog.dismiss();
                     Toast.makeText(SingInActivity.this, "Please fill up the fields",
