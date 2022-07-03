14763d7444408b187b752d9d420a19bfd84dd86b
r.ruscittomarinel@studenti.unimol.it <p3p-C/?maxx/CgF>
2022-07-03 16:33:10 +0200

    fix

--- app/src/main/java/com/example/dazero/SearchFragment.java
+++ app/src/main/java/com/example/dazero/SearchFragment.java
@@ -18,7 +18,7 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_main, container, false);
+        return inflater.inflate(R.layout.activity_sing_in, container, false);
     }
 
 
